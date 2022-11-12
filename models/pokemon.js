5ac1ce6051bdf4dd29f2c633ee8ef177566704ca
Mary Sanni <marysanni01@gmail.com>
2022-11-12 17:32:41 -0600

    commit

---
+++ models/pokemon.js
@@ -0,0 +1,195 @@
+const pokemon=[
+
+    {
+        id: "001",
+        name: "Bulbasaur",
+        img: "http://img.pokemondb.net/artwork/bulbasaur.jpg",
+        type: [
+          "Grass",
+          "Poison"
+        ],
+    },
+    {
+        id: "002",
+        name: "Ivysaur",
+        img: "http://img.pokemondb.net/artwork/ivysaur.jpg",
+        type: [
+          "Grass",
+          "Poison"
+        ],
+    },
+    {
+        id: "002",
+        name: "Ivysaur",
+        img: "http://img.pokemondb.net/artwork/ivysaur.jpg",
+        type: [
+          "Grass",
+          "Poison"
+        ],
+  
+    },
+  
+  {
+    id: "004",
+    name: "Charmander",
+    img: "http://img.pokemondb.net/artwork/charmander.jpg",
+    type: [
+      "Fire"
+    ],
+  },
+  
+  {
+    id: "005",
+    name: "Charmeleon",
+    img: "http://img.pokemondb.net/artwork/charmeleon.jpg",
+    type: [
+      "Fire"
+    ],
+  },
+  
+  {
+    id: "006",
+    name: "Charizard",
+    img: "http://img.pokemondb.net/artwork/charizard.jpg",
+    type: [
+      "Fire",
+      "Flying"
+    ],
+  },
+  {
+    id: "007",
+    name: "Squirtle",
+    img: "http://img.pokemondb.net/artwork/squirtle.jpg",
+    type: [
+      "Water"
+    ],
+  },
+  {
+    id: "008",
+    name: "Wartortle",
+    img: "http://img.pokemondb.net/artwork/wartortle.jpg",
+    type: [
+      "Water"
+    ],
+  },
+  {
+    id: "009",
+    name: "Blastoise",
+    img: "http://img.pokemondb.net/artwork/blastoise.jpg",
+    type: [
+      "Water"
+    ],
+  },
+  {
+    id: "010",
+    name: "Caterpie",
+    img: "http://img.pokemondb.net/artwork/caterpie.jpg",
+    type: [
+      "Bug"
+    ],
+  },
+  {
+    id: "011",
+    name: "Metapod",
+    img: "http://img.pokemondb.net/artwork/metapod.jpg",
+    type: [
+      "Bug"
+    ],
+  },
+  {
+    id: "013",
+    name: "Weedle",
+    img: "http://img.pokemondb.net/artwork/weedle.jpg",
+    type: [
+      "Bug",
+      "Poison"
+    ],
+  },
+  {
+    id: "014",
+    name: "Kakuna",
+    img: "http://img.pokemondb.net/artwork/kakuna.jpg",
+    type: [
+      "Bug",
+      "Poison"
+    ],
+  },
+  {
+    id: "015",
+    name: "Beedrill",
+    img: "http://img.pokemondb.net/artwork/beedrill.jpg",
+    type: [
+      "Bug",
+      "Poison"
+    ],
+  },
+  {
+    id: "016",
+    name: "Pidgey",
+    img: "http://img.pokemondb.net/artwork/pidgey.jpg",
+    type: [
+      "Normal",
+      "Flying"
+    ],
+  },
+  {
+    id: "017",
+    name: "Pidgeotto",
+    img: "http://img.pokemondb.net/artwork/pidgeotto.jpg",
+    type: [
+      "Normal",
+      "Flying"
+    ],
+  },
+  {
+    id: "018",
+    name: "Pidgeot",
+    img: "http://img.pokemondb.net/artwork/pidgeot.jpg",
+    type: [
+      "Normal",
+      "Flying"
+    ],
+  },
+  {
+    id: "019",
+    name: "Rattata",
+    img: "http://img.pokemondb.net/artwork/rattata.jpg",
+    type: [
+      "Normal"
+    ],
+  },
+  {
+    id: "020",
+    name: "Raticate",
+    img: "http://img.pokemondb.net/artwork/raticate.jpg",
+    type: [
+      "Normal"
+    ],},
+    {
+        id: "021",
+        name: "Spearow",
+        img: "http://img.pokemondb.net/artwork/spearow.jpg",
+        type: [
+          "Normal",
+          "Flying"
+        ],},
+        {
+            id: "022",
+            name: "Fearow",
+            img: "http://img.pokemondb.net/artwork/fearow.jpg",
+            type: [
+              "Normal",
+              "Flying"
+            ],},
+            
+  ]
+  module.exports=pokemon
+    
+  
+  
+  
+  
+  
+  
+  
+    
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express= require('express');
 const app=express();
 const pokemon= require('./models/pokemon.js');
 const { response } = require('express');
+app.use(express.urlencoded({extended: true}));
+
 
 
 
@@ -10,6 +12,14 @@ app.get('/pokemon', (request, response)=>{
     allpokemon:pokemon
  })
 });
+app.get('/pokemon/:id /edit',(request, response)=>{
+
+    response.render('index.ejs', {allpokemon:pokemon});
+})
+
+app.get('/pokemon/:id', (request, response)=>{
+    response.render("show.ejs",{pokemonname : pokemon[request.params.id].name, pokemonimg:pokemon[request.params.id].img, pokemontype:pokemon[request.params.id].type })
+})
 app.listen(3000,()=>{
     console.log("serveris listening to port...")
 });
